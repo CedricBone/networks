ba277be884ad9cd9983dad69c3aead67f0a93a5b
CedricBone <70604834+CedricBone@users.noreply.github.com>
2025-03-17 15:10:16 -0400

    adding classes and skeleton code

--- cedric_bone_hw3/utils.py
+++ cedric_bone_hw3/utils.py
@@ -8,13 +8,35 @@ class Packet:
         self.data = data 
         self.checksum = checksum
         self.ack_num = ack_num 
+    
+    def calculate_checksum(self):
+        return sum(self.data)
 
-class sender:
-    ...
+    
 
-class receiver:
-    ...
+class Sender:
+    def __init__(self, window_size):
+        self.window_size = window_size
+        self.window = []
+        self.seq_num = 0
+        self.ack_num = 0
+    
+    def send_packet(self, data):
+        pass
+
+class Receiver:
+    def __init__(self, window_size):
+        self.window_size = window_size
+        self.window = []
+        self.ack_num = 0
+        self.seq_num = 0
+    
+    def receive_packet(self, packet):
+        pass
 
 class NetworkSimulator:
-    ...
+    def __init__(self, loss_rate, corruption_rate, delay):
+        self.loss_rate = loss_rate
+        self.corruption_rate = corruption_rate
+        self.delay = delay
 
